8a1e73c630ae9a383c9a66e5c5ed79a0e08def56
poly1234 <methesteam@gmail.com>
2026-05-13 17:51:32 +0900

    Update MD_system_questions.sql

--- MD_system_questions.sql
+++ MD_system_questions.sql
@@ -139,3 +139,9 @@ SELECT 극장.극장이름, 상영관.영화제목, 예약.날짜 FROM 극장 JO
 SELECT 고객.이름, 예약.날짜 FROM 고객 JOIN 예약 ON 고객.고객번호 = 예약.고객번호;
 SELECT 극장.극장이름, 상영관.영화제목, 고객.이름, 예약.좌석번호 FROM 극장 JOIN 상영관 ON 극장.극장번호 = 상영관.극장번호 JOIN 예약 ON 상영관.극장번호 = 예약.극장번호 AND 상영관.상영관번호 = 예약.상영관번호 JOIN 고객 ON 예약.고객번호 = 고객.고객번호;
 SELECT 상영관.영화제목, COUNT(*) AS 총예약수 FROM 상영관 JOIN 예약 ON 상영관.극장번호 = 예약.극장번호 AND 상영관.상영관번호 = 예약.상영관번호 GROUP BY 상영관.영화제목;
+
+SELECT 상영관.영화제목, 상영관.가격 FROM 극장 JOIN 상영관 ON 극장.극장번호 = 상영관.극장번호 WHERE 극장.위치 = '서울';
+SELECT 고객.이름 FROM 고객 LEFT JOIN 예약 ON 고객.고객번호 = 예약.고객번호 WHERE 예약.극장번호 IS NULL;
+SELECT 극장.극장이름, COUNT(*) AS 총예약수 FROM 극장 JOIN 상영관 ON 극장.극장번호 = 상영관.극장번호 JOIN 예약 ON 상영관.극장번호 = 예약.극장번호 AND 상영관.상영관번호 = 예약.상영관번호 GROUP BY 극장.극장이름;
+SELECT 예약.고객번호, 상영관.영화제목 FROM 상영관 JOIN 예약 ON 상영관.극장번호 = 예약.극장번호 AND 상영관.상영관번호 = 예약.상영관번호 WHERE 상영관.가격 >= 15000;
+SELECT 고객.이름, COUNT(*) AS 총예약횟수 FROM 고객 JOIN 예약 ON 고객.고객번호 = 예약.고객번호 GROUP BY 고객.이름;
